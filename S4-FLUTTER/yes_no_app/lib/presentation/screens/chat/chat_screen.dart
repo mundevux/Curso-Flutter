b5cd3edddcfa8eaea6998a8ba64698fc4f4507ca
Camilo Cuitiva <camiloandres174@gmail.com>
2023-11-30 16:51:42 -0500

    Add custom color theme to app

---
+++ S4-FLUTTER/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://e00-marca.uecdn.es/assets/multimedia/imagenes/2023/05/09/16836549959307.jpg'),
+          ),
+        ),
+        title: const Text('Mi Amor 🤍'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return const MyMessageBubble();
+                    })),
+            Text('Mundo')
+          ],
+        ),
+      ),
+    );
+  }
+}
